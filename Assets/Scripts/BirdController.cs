2dd2b3114253f533d312c7dcffc4b0dcdafae633
emiruyan <emir-uyn@hotmail.com>
2022-08-25 16:59:16 +0300

    Game Over Panel
    The Game Over Panel and Play Again Button, which will appear when we die, have been added to the game.

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class BirdController : MonoBehaviour
@@ -10,9 +11,16 @@ public class BirdController : MonoBehaviour
     
     [SerializeField] private Text scoreText;
 
+    [SerializeField] private GameObject gameOverPanel;
+
     private int score;
 
-    
+
+    private void Start()
+    {
+        Time.timeScale = 1; //Öldükten sonra tekrar oynayabilmek için timeScale'i Startta tekrar 1'e atıyoruz
+    }
+
 
     private void Update()
     {
@@ -22,12 +30,27 @@ public class BirdController : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D col) 
+    private void OnTriggerEnter2D(Collider2D col)  //Score işlemi için OnTriggerEnter ve "Tag" yönetimini kullandık.
     {
-        if (col.tag == "Score") //tetiklenen colliderın tagı "Score" ise;
+        if (col.tag == "Score") //tetiklenen game object'ın tagı "Score" ise;
         {
             score++; //score'u bir arttır
             scoreText.text = score.ToString(); //scoreText'in içerisindeki texte yazdır ve ToString'e çevir.
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D col) // Death işlemi için OnCollisionEnter ve "Tag" yöntemini kullandık.
+    {
+        if (col.gameObject.tag == "Death") //çarpışan gameObject'ın tagı "Death" ise
+        {
+            Time.timeScale = 0; //timeScale ağır çekimdir ve burada 0'a atayıp oyunu durduruyoruz.
+            gameOverPanel.SetActive(true);
+        }
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);  
+    }
+    
 }
